ce84f5d3146b3c5e4186d3a7773e39af98aa04a0
ofarkas00 <jjulialeao@gmail.com>
2026-04-10 08:11:57 -0300

    aula 10/04

---
+++ app_04/login/app/index.tsx
@@ -0,0 +1,19 @@
+import {View, Text} from 'react-native'
+
+export default function Index () {
+    return(
+        <view            
+
+    )
+    
+}
+const style = StyleSheet.create({
+    container: (
+        flex:1,
+        padding: 32
+    ),
+        título: {
+            fontSize: 32,
+            fontWeight: 900
+        }
+})
